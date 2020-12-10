1dbee4b45f396f3d5aab1fe4a073f2c5ebe3406c
Petr Aubrecht st. <petr@aubrecht.net>
2020-12-10 11:34:28 +0100

    improve docker script to automatically remove old container before each run, don't run as a deamon

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name sample-javascript-nameday -v $(pwd)/public_html/:/usr/share/nginx/html/:ro -d  -p 8080:80 nginx:alpine
+docker run --name sample-javascript-nameday -v $(pwd)/public_html/:/usr/share/nginx/html/:ro -p 8080:80 --rm nginx:alpine
